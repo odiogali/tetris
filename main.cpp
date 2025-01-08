958516c82ef32ad2fc8427145d2389b72a2ba65b
Odi Ogali <odi.ogali1380@gmail.com>
2025-01-08 01:32:51 -0700

    Fixed the redrawing problem: needed to clear screen before drawing new color - also added in the corneflower blue background for the triangle again.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 const char* vertexShaderSource = "#version 330 core\n"
   "layout (location = 0) in vec3 aPos;\n"
@@ -52,11 +56,11 @@ int main(){
     0.0f, 0.5f, 0.0f,
   };
 
+  // build and compile vertex shader
   unsigned int vertexShader;
   vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
   glCompileShader(vertexShader);
-  
   // Check for compile-time error regarding the vertex shader
   int success;
   char infoLog[512];
@@ -66,11 +70,19 @@ int main(){
     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
 
+  // build and compile fragment shader
   unsigned int fragmentShader;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
   glCompileShader(fragmentShader);
+  // Check for compilation errors for the fragment shader
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success){
+    glGetShaderInfoLog(fragmentShader, 512,  NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
 
+  // link the shaders into one shader program
   unsigned int shaderProgram;
   shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
@@ -95,6 +107,7 @@ int main(){
   // Copy our vertices array in a buffer for OpenGL to use
   glBindBuffer(GL_ARRAY_BUFFER, VBO); // we are to work on the 'VBO' with the object ID in VBO
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
   // Set our vertex attributes pointers
   // Param 1: specifies which vertex attrib we want to configure - we specified location of position vertex attribute
   // in vertex shader with 'layout (location = 0)' - sets the location of the vertex attrib to 0; since we want to 
@@ -108,18 +121,32 @@ int main(){
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
   glEnableVertexAttribArray(0);
 
+  // Unbind registered VBO
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  // Unbind registered VAO so that other VAO calls don't accidentally modify this VAO; in general, don't unbind VAOs
+  // or VBOs unless directly necessary
+  glBindVertexArray(0);
+
   while(!glfwWindowShouldClose(window)){
-    processInput(window);
+    processInput(window);    
+
+    // Draw blue background
+    glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    // Draw the object
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  // Deallocate used resources
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
 
   glfwTerminate();
   return 0;

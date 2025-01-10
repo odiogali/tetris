f6876b954f6623a20a741e866bfa27ebaee0200b
Odi Ogali <odi.ogali1380@gmail.com>
2025-01-10 01:53:03 -0700

    Glowing green triangle - color passed in by uniform GLSL datatype.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 #include <iostream>
 
@@ -8,20 +9,17 @@ void processInput(GLFWwindow* window);
 
 const char* vertexShaderSource = "#version 330 core\n"
   "layout (location = 0) in vec3 aPos;\n"
+  "out vec4 vertexColor;\n"
   "void main(){\n"
-  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  " gl_Position = vec4(aPos, 1.0);\n"
+  " vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
   "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
   "out vec4 FragColor;\n"
+  "uniform vec4 ourColor;\n"
   "void main(){\n"
-  " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-  "}\0";
-
-const char* secondFragmentShaderSource = "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "void main(){\n"
-  " FragColor = vec4(0.972f, 0.784f, 0.024f, 1.0f);\n"
+  " FragColor = ourColor;\n"
   "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
@@ -55,6 +53,12 @@ int main(){
   // x = 0 starts from left, y = 0 starts from bottom
   glViewport(0,0,800,600); // glViewPort(x, y, width, height)
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f,
+  };
 
   // build and compile vertex shader
   unsigned int vertexShader;
@@ -95,62 +99,37 @@ int main(){
     std::cout << "ERROR::SHADER::LINKING::LINK_FAILED\n" << infoLog << std::endl;
   }
 
-  // Second shader program
-  // Rebuild and recompile second fragment shader
-  unsigned int secondFragmentShader;
-  secondFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(secondFragmentShader, 1, &secondFragmentShaderSource, NULL);
-  glCompileShader(secondFragmentShader);
-  unsigned int secondShaderProgram = glCreateProgram();
-  glAttachShader(secondShaderProgram, vertexShader);
-  glAttachShader(secondShaderProgram, secondFragmentShader);
-  glLinkProgram(secondShaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
-  float firstTriVertices[] = {
-    0.5f, 0.75f, 0.0f,  // right top point
-    0.5f, -0.75f, 0.0f, // right bottom point
-    0.0f, -0.75f, 0.0f,  // shared mid point
-  };
-  float secondTriVertices[] = {
-    0.0f, -0.75f, 0.0f,  // shared mid point
-    -0.5f, -0.75f, 0.0f, // left bottom point
-    -0.5f, 0.75f, 0.0f,  // left top point
-  };
-  //unsigned int indices[] = { // start from 0
-  //  0, 1, 2, // first triangle
-  //  2, 3, 4 // second triangle
-  //};
+  unsigned int VBO;
+  glGenBuffers(1, &VBO); // generate a single vertex buffer object and store the object ID in VBO
 
-  unsigned int VAO1;
-  glGenVertexArrays(1, &VAO1);
-  glBindVertexArray(VAO1);
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  // Copy our vertices array in a buffer for OpenGL to use
+  glBindBuffer(GL_ARRAY_BUFFER, VBO); // we are to work on the 'VBO' with the object ID in VBO
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  unsigned int VBO1;
-  glGenBuffers(1, &VBO1);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriVertices), firstTriVertices, GL_STATIC_DRAW);
-  //unsigned int EBO;
-  //glGenBuffers(1, &EBO);
-  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  // Set our vertex attributes pointers
+  // Param 1: specifies which vertex attrib we want to configure - we specified location of position vertex attribute
+  // in vertex shader with 'layout (location = 0)' - sets the location of the vertex attrib to 0; since we want to 
+  // pass data to this vertex attribute, we pass in 0
+  // Param 2: specifies the size of the vertex attrib - a vec3 thus it is composed of 3 values
+  // Param 3: specifies the type of the data: GL_FLOAT (a vec* in GLSL consists of floating point values)
+  // Param 5: known as stride and tells us the space between consecutive vertex attrib - 3 times the size of a float
+  // but we could've also just used 0 since the vertices are tightly packed
+  // Param 6: the offset of where the position data begins in the buffer - since pos data is at the start of the data
+  // array, this value is just 0. The type of this parameter is void*
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
   glEnableVertexAttribArray(0);
 
+  // Unbind registered VBO
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  // Unbind registered VAO so that other VAO calls don't accidentally modify this VAO; in general, don't unbind VAOs
+  // or VBOs unless directly necessary
   glBindVertexArray(0);
-
-  unsigned int VAO2;
-  glGenVertexArrays(1, &VAO2);
-  glBindVertexArray(VAO2);
-
-  unsigned int VBO2;
-  glGenBuffers(1, &VBO2);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriVertices), secondTriVertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
-
-  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   while(!glfwWindowShouldClose(window)){
     processInput(window);    
@@ -159,15 +138,14 @@ int main(){
     glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Draw the shape
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO1);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glBindVertexArray(0);
+    float timeValue = glfwGetTime();
+    float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-    glUseProgram(secondShaderProgram);
-    glBindVertexArray(VAO2);
+    glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // check and call events and swap the buffers
@@ -176,9 +154,8 @@ int main(){
   }
 
   // Deallocate used resources
-  //glDeleteVertexArrays(1, &VAO);
-  //glDeleteBuffers(1, &VBO);
-  //glDeleteBuffers(1, &EBO);
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
   glDeleteProgram(shaderProgram);
 
   glfwTerminate();
